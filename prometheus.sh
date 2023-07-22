d3a19613bf3104fac4be80ca32921046329aff43
mcjahn <131519880+mcjahn@users.noreply.github.com>
2023-07-22 16:55:50 +0700

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+yum install -y git mc
+git clone https://github.com/mcjahn/otus2.git
 #качаем дистрибутив Prometheus
 wget https://github.com/prometheus/prometheus/releases/download/v2.45.0/prometheus-2.45.0.linux-amd64.tar.gz
 #создаем директорию куда переместим дистрибутив
